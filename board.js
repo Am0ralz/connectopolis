145ebe4ba69a8adb4c1499e4006835f3d7f098aa
Jas <c.jasminher@gmail.com>
2020-10-24 21:20:18 -0400

    Removed objects and moved them to a different file

--- board.js
+++ board.js
@@ -1,4 +1,5 @@
 import { data } from "./info.js";
+import { DiffTree, TrafficLight } from "./objects.js";
 
 ZIMONON = true;
 
@@ -18,39 +19,9 @@ frame.on("ready", () => {
   const stageW = frame.width;
   const stageH = frame.height;
 
-  // creates class for traffic light object
-  var TrafficLight = function () {
-    this.super_constructor();
-    this.type = "TrafficLight";
-    this.arguments = arguments;
 
-    const post = new Rectangle(10, 80, black).centerReg().loc(-5, -40, this); //post for traffic light
-    new Circle(10 / 2, black).sca(1, 0.3).centerReg(post).mov(0, 40); //small disk under post to give illusion of 3d post
-    new Rectangle(30, 50, black).centerReg().loc(-5, -65, this); //traffic  light
-    new Circle(30 / 2.5, black).sca(1, 0.3).loc(-5, -90, this); //small disk above traffic light
-    new Circle(30 / 2.5, black).sca(1, 0.3).loc(-5, -39, this); //small disk under traffic light
-
-    new Circle(6, red).loc(-5, -80, this); //red traffic light
-    new Circle(6, yellow).loc(-5, -64, this); //yellow traffic light
-    new Circle(6, green).loc(-5, -48, this); //green traffic light
-  };
   extend(TrafficLight, Container);
-
-// creates class for different tree object
-var DiffTree = function(){
-  this.super_constructor();
-  this.type = "DiffTree";
-  this.arguments = arguments;
-
-  new Circle(10,"#764A23").sca(1,.5).addTo(this);
-  this.centerReg(null,null,false);
-  new Rectangle(20,50, "#764A23").loc(-10,-50,this);
-  new Circle(rand(35,45),"#81B721").sca(1,.65).loc(0,-50,this);
-  new Circle(rand(20,30), "#81B721").sca(1,1).loc(-5,-70,this);
-  new Circle(rand(15,20), "#81B721").sca(1,1).loc(12,-72,this);
-}
-
-extend(DiffTree, Container);
+  extend(DiffTree, Container);
 
 
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
